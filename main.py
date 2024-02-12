9b11619bd36c5cdeefe4bbfc19a4bc91152647ee
kathalapuramapurva <kathalapuramapurva414@gmail.com>
2024-02-12 10:55:12 +0530

    hello

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('index1.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+    return render_template("success_html")
+ 
+
+if __name__ == "__main__":
+    print("Apurva")
+    app.run()
